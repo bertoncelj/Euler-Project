32f8f39221c354f2464b402a44e2b5d7c8e5ffe4
Tine Bertoncelj <tine.bertoncelj@gmail.com>
2016-03-08 15:10:03 +0100

    Upload first base code

--- 5/problem5.c
+++ 5/problem5.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(){
-	printf("To je problem 5");
-	return 0;
+int main()
+{
+   int n, i = 3, count, c;
+   int maxPrime=10 001;
+ 
+
+ 
+   for ( count = 2 ; count <= maxPrime ;  )
+   {
+      for ( c = 2 ; c <= i - 1 ; c++ )
+      {
+         if ( i%c == 0 )
+            break;
+      }
+            if ( c == i )
+      {
+         printf("%d\n",i);
+         count++;
+      }
+      i++;
+   }
+ 
+   return 0;
 }

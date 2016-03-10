0924f8dcbe179ad5a230d513f71372375836435e
Tine Bertoncelj <tine.bertoncelj@gmail.com>
2016-03-11 00:50:42 +0100

    Finale code Not optimize! Could be must faster

---
+++ Problem 7/problem7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+   int n, i = 3, count, c;
+   int maxPrime = 10001;
+ 
+
+ 
+   for ( count = 2 ; count <= maxPrime ;  )
+   {
+      for ( c = 2 ; c <= i - 1 ; c++ )
+      {
+         if ( i%c == 0 )break;
+      }
+            if ( c == i )
+      {
+         printf("%d\n",i);
+         count++;
+      }
+      i++;
+   }
+ 
+   return 0;
+}

a24a748941a14eef2fc785db8eced60331c78fd2
tine <bertotine@gmail.com>
2023-04-29 00:50:00 +0100

    Prob2 make fibbonaci with accumulator and filter functions

--- Hs/d2.hs
+++ Hs/d2.hs
@@ -1,20 +1,9 @@
-fib :: Int -> Int
-fib 0 = 0
-fib 1 = 1
-fib x = fib (x - 1) + fib (x - 2)
-
--- createList :: Int -> [Int]
--- createList topMax
--- \| 4 000 000 > topMax = x:xs
---
-fib_list = takeWhile (<= 4000000) [fib (x) | x <- [1 ..]]
-
-filter_even :: Int -> Int
-filter_even x
-  | even x = x
-  | otherwise = 0
-
-fib_evens = map filter_even fib_list
+fibA :: Int -> [Int]
+fibA maxVal = fibAcc 0 1
+  where
+    fibAcc a b
+      | a > maxVal = []
+      | otherwise = a : fibAcc b (a + b)
 
 main :: IO ()
-main = print $ sum $ fib_evens
+main = print $ sum $ filter (even) (fibA 4000000)
